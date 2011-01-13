e63d1acf98deb9b93275047e1169fad2d13037cb
viruscamp <viruscamp@gmail.com>
2011-01-13 15:59:01 +0800

    ArchDB-1.0.zip Initial version.

---
+++ ArchDB.lua
@@ -0,0 +1,293 @@
+ArchDB = LibStub("AceAddon-3.0"):NewAddon("ArchDB", "AceEvent-3.0", "AceTimer-3.0", "AceConsole-3.0");
+
+local AceGUI = LibStub("AceGUI-3.0");
+
+ArchDB.ADB = {};
+local ADB = ArchDB.ADB;
+
+local appName = ...
+
+ADB.DEBUG_MODE = false
+ADB.VERSION = GetAddOnMetadata(appName, "Version");
+ADB.IconFormat = "|T%s:20:20|t";
+
+ADB.ArtifactList = {};
+
+ADB.ArtifactCnt = 0
+ADB.Scroll = nil
+
+ADB.ItemColors = {
+	"ff9d9d9d", -- gray (crappy) 
+	"ffffffff", -- white (normal)
+	"ff1eff00", -- green (uncommon)
+	"ff0070dd", -- blue (rare)
+	"ffa335ee", -- purple (epic)
+	"ffff8000", -- orange (legendary)
+	"ffe5cc80", -- beige (artifact)
+};
+
+local defaults = {
+	profile = {
+		Scale = 1,
+	},
+	char = {
+		LastVersion = ADB.VERSION,
+		LastSelected = 1;
+		ShowAll = true;
+	},
+}
+
+function ADB.Debug(...)
+	if(ADB.DEBUG_MODE) then
+		local output, part
+		output = "ArchDB: ";
+		for i=1, select("#", ...) do
+			part = select(i, ...)
+			part = tostring(part):gsub("{{", "|cffddeeff"):gsub("}}", "|r")
+			if (output) then 
+				output = output .. " " .. part
+			else 
+				output = part 
+			end
+		end
+		ChatFrame1:AddMessage(output, 1.0, 1.0, 1.0);
+	end
+end
+
+function ArchDB:RegisterSlashCommands()
+	self:RegisterChatCommand("archdb", "ChatCmd");
+end
+
+function ArchDB:AddArtifact(frame, info)
+	local itemName = info[1];
+	local itemLink = info[2];
+	local itemRarity = info[3];
+	local icon = info[4];
+	local completed = info[5];
+
+	local label = AceGUI:Create("InteractiveLabel");
+	label:SetText(string.format(ADB.IconFormat, icon).."|c"..ADB.ItemColors[itemRarity+1]..itemName.." ("..completed..")|r");
+	label:SetWidth(210);
+	label:SetCallback("OnEnter", function () ShowUIPanel(GameTooltip)
+		GameTooltip:SetOwner(UIParent, "ANCHOR_CURSOR")
+		GameTooltip:SetHyperlink(itemLink);
+		GameTooltip:Show() end);
+	label:SetCallback("OnLeave", function () ShowUIPanel(GameTooltip)
+		GameTooltip:Hide() end);
+
+	frame:AddChild(label);
+
+end
+
+function ADB.AddCount(raceIndex, name, cnt)
+	for i, j in pairs(ADB.ArtifactList[raceIndex]) do
+		if ADB.ArtifactList[raceIndex][i][1] == name then
+			ADB.ArtifactList[raceIndex][i][5] = cnt;
+			return;
+		end
+	end
+end
+
+function ADB.GetArtifactCounts(raceIndex)
+	local artifactIndex;
+	ADB.ArtifactCnt = 0;
+
+	local artifactCount = GetNumArtifactsByRace(raceIndex);
+
+	for artifactIndex=1, artifactCount do
+		local artifactName, artifactDescription, artifactRarity, artifactIcon, hoverDescription, keystoneCount, bgTexture, firstCompletionTime, completionCount = GetArtifactInfoByRace(raceIndex, artifactIndex);
+		ADB.AddCount(raceIndex, artifactName, completionCount);
+		ADB.ArtifactCnt = ADB.ArtifactCnt + completionCount;
+	end
+end
+
+function ADB.SetButtonText(button)
+	if ADB.db.char.ShowAll == true then
+		button:SetText("Show Unknown");
+	else
+		button:SetText("Show All");
+	end
+end
+
+function ADB.PopulateWindow(key)
+	local raretotal = 0;
+	local rareknown = 0;
+	local commontotal = 0;
+	local commonknown = 0;
+	local lastrarity = -1;
+
+	ADB.Scroll = AceGUI:Create("ScrollFrame");
+	ADB.Scroll:SetLayout("Flow");
+	
+	local button = AceGUI:Create("Button");
+	ADB.SetButtonText(button);
+
+	button:SetCallback("OnClick", function(self, event, key) 
+		ADB.db.char.ShowAll = not ADB.db.char.ShowAll;
+		ADB.Group:ReleaseChildren();
+		ADB.PopulateWindow(ADB.db.char.LastSelected) end);
+
+	ADB.Scroll:AddChild(button);
+
+	ADB.GetArtifactCounts(key);
+ 	
+	for i, j in pairs(ADB.ArtifactList[key]) do
+		if j[3] > 2 then 
+			raretotal = raretotal + 1; 
+			if ADB.ArtifactList[key][i][5] > 0 then
+				rareknown = rareknown + 1;
+			end
+		else 
+			commontotal = commontotal + 1;
+			if ADB.ArtifactList[key][i][5] > 0 then
+				commonknown = commonknown + 1;
+			end
+		end
+	end
+
+	local rares = AceGUI:Create("Heading");
+	rares:SetFullWidth(true)
+	rares:SetText("Rares ("..rareknown.." of "..raretotal.." Known)");
+	ADB.Scroll:AddChild(rares);
+	
+	for i, j in pairs(ADB.ArtifactList[key]) do
+		if (ADB.db.char.ShowAll == true or (ADB.db.char.ShowAll == false and ADB.ArtifactList[key][i][5] == 0)) and j[3] > 2 then
+			if lastrarity ~= -1 and lastrarity ~= j[3] then
+				-- Add spacer
+				local spacer = AceGUI:Create("Label");
+				spacer:SetFullWidth(true);
+				ADB.Scroll:AddChild(spacer);
+			end
+			ArchDB:AddArtifact(ADB.Scroll, j);
+			lastrarity = j[3];
+		end
+	end
+
+	local commons = AceGUI:Create("Heading");
+	commons:SetText("Commons ("..commonknown.." of "..commontotal.." Known)");
+	commons:SetFullWidth(true)
+	ADB.Scroll:AddChild(commons);
+
+	for i, j in pairs(ADB.ArtifactList[key]) do
+		if (ADB.db.char.ShowAll == true or (ADB.db.char.ShowAll == false and ADB.ArtifactList[key][i][5] == 0)) and j[3] <= 2 then
+			ArchDB:AddArtifact(ADB.Scroll, j);
+		end
+	end
+
+
+	ADB.Group:AddChild(ADB.Scroll);
+	ADB.db.char.LastSelected = key;
+end
+
+function ArchDB:BuildArtifacts(raceIndex)
+	local itemid;
+	local ret = true;
+
+	ADB.ArtifactList[raceIndex] = {};
+
+	for _, itemid in pairs(ArchDB_ArtifactList[raceIndex]) do
+		local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(itemid);
+		if itemName == nil then 
+			ret = false;
+		else
+			table.insert(ADB.ArtifactList[raceIndex], 1, { itemName, itemLink, itemRarity, itemTexture, 0 }); 
+		end
+	end
+
+	return ret;
+end
+
+function ArchDB:BuildData()
+	if ADB.DataBuilt ~= nil then return true end
+
+	local raceCount = GetNumArchaeologyRaces()
+	local raceIndex;
+
+	local ret = true;
+
+	ADB.Races = {};
+	for raceIndex=1, raceCount do
+		local raceName, raceCurrency, raceTexture, raceItemID = GetArchaeologyRaceInfo(raceIndex);
+		if raceName == nil then 
+			ADB.Debug("No race name for race id "..raceIndex);
+			ret = false;
+		end
+		if raceName ~= nil and raceName ~= "Other" then 
+			tinsert(ADB.Races, raceIndex, raceName);
+			ArchDB_ArtifactList_Setup(raceIndex, raceName);
+			if ArchDB:BuildArtifacts(raceIndex) == false then
+				ret = false;
+			else
+				table.sort(ADB.ArtifactList[raceIndex], function(a, b) 
+					if a[3] > b [3] then return 1 end;
+					if a[3] < b [3] then return nil end;
+					return a[1] < b[1] 
+				end);
+			end
+		end
+	end
+	if ret == true then 
+		ADB.DataBuilt = 1; 
+	end
+	return ret;
+end
+
+function ADB.SetGroupTitle()
+	local title = ADB.Races[ADB.db.char.LastSelected];
+	title = title.." ("..ADB.ArtifactCnt.." Total Completed)";
+	ADB.Group:SetTitle(title);
+end
+
+function ArchDB:BuildWindow()
+	if ArchDB:BuildData() == false then
+		ArchDB:ScheduleTimer("BuildWindow", 0.1);
+		return;
+	end
+
+	local frame = AceGUI:Create("Frame")
+	frame:SetTitle("ArchDB")
+	frame:SetStatusText("Archaeology Database");
+	frame:SetCallback("OnClose", function(widget) ADB.Scroll = nil; AceGUI:Release(widget) end)
+	frame:SetLayout("Fill")
+
+	ADB.Group = AceGUI:Create("DropdownGroup")
+	ADB.Group:SetGroupList(ADB.Races)
+	ADB.Group:SetGroup(ADB.db.char.LastSelected);
+	ADB.Group:SetLayout("Fill")
+	ADB.Group:SetCallback("OnGroupSelected", function(self, event, key) 
+			ADB.Group:ReleaseChildren();
+			ADB.PopulateWindow(key)
+			ADB.SetGroupTitle();
+		end);
+	frame:AddChild(ADB.Group);
+
+	ADB.PopulateWindow(ADB.db.char.LastSelected);
+	ADB.SetGroupTitle();
+
+	_G["ArchDBFrame"] = frame;
+	tinsert(UISpecialFrames, "ArchDBFrame");
+	frame:Show();
+end
+
+function ArchDB:ChatCmd(args)
+	ArchDB:BuildWindow();
+end
+
+function ArchDB:OnInitialize()
+	ADB.db = LibStub("AceDB-3.0"):New("ArchDBDB", defaults, true);
+--	ADB.db.ResetDB();
+
+	ArchDB:RegisterSlashCommands();
+end
+
+function ArchDB:ArtifactHistoryReady()
+	if ADB.Scroll ~= nil then 
+		ADB.Group:ReleaseChildren();
+		ADB.PopulateWindow(ADB.db.char.LastSelected);
+	end
+end
+
+function ArchDB:OnEnable()
+	RequestArtifactCompletionHistory();
+	ArchDB:RegisterEvent("ARTIFACT_HISTORY_READY", "ArtifactHistoryReady");
+end
---
+++ Artifacts.lua
@@ -0,0 +1,214 @@
+ArchDB_ArtifactList = {};
+
+
+
+local function ADB_Dwarf(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+64373, -- 'Chalice of the Mountain Kings'
+64372, -- 'Clockwork Gnome'
+64489, -- 'Staff of Sorcerer-Thane Thaurissan'
+64488, -- 'The Innkeeper\'s Daughter'
+
+63113, -- 'Belt Buckle with Anvilmar Crest'
+64339, -- 'Bodacious Door Knocker'
+63112, -- 'Bone Gaming Dice'
+64340, -- 'Boot Heel with Scrollwork'
+63409, -- 'Ceramic Funeral Urn'
+64362, -- 'Dented Shield of Horuz Killcrow'
+66054, -- 'Dwarven Baby Socks'
+64342, -- 'Golden Chamber Pot'
+64344, -- 'Ironstar\'s Petrified Shield'
+64368, -- 'Mithril Chain of Angerforge'
+63414, -- 'Moltenfist\'s Jeweled Goblet'
+64337, -- 'Notched Sword of Tunadil the Redeemer'
+63408, -- 'Pewter Drinking Cup'
+64659, -- 'Pipe of Franclorn Forgewright'
+64487, -- 'Scepter of Bronzebeard'
+64367, -- 'Scepter of Charlga Razorflank'
+64366, -- 'Scorched Staff of Shadow Priest Anund'
+64483, -- 'Silver Kris of Korl'
+63411, -- 'Silver Neck Torc'
+64371, -- 'Skull Staff of Shadowforge'
+64485, -- 'Spiked Gauntlets of Anvilrage'
+63410, -- 'Stone Gryphon'
+64484, -- 'Warmaul of Burningeye'
+64343, -- 'Winged Helm of Corehammer'
+63111, -- 'Wooden Whistle'
+64486, -- 'Word of Empress Zoe'
+63110, -- 'Worn Hunting Knife'
+};
+end
+
+local function ADB_Draenei(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+64456, -- 'Arrival of the Naaru'
+64457, -- 'The Last Relic of Argus'
+
+64440, -- 'Anklet with Golden Bells'
+64453, -- 'Baroque Sword Scabbard'
+64442, -- 'Carved Harp of Exotic Wood'
+64455, -- 'Dignified Portrait'
+64454, -- 'Fine Crystal Candelabra'
+64458, -- 'Plated Elekk Goad'
+64444, -- 'Scepter of the Nathrezim'
+64443, -- 'Strange Silver Paperweight'
+};
+end
+
+local function ADB_Fossil(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+60955, -- 'Fossilized Hatchling'
+60954, -- 'Fossilized Raptor'
+
+64355, -- 'Ancient Shark Jaws'
+63121, -- 'Beautiful Preserved Fern'
+63109, -- 'Black Trilobite'
+64349, -- 'Devilsaur Tooth'
+64385, -- 'Feathered Raptor Arm'
+64473, -- 'Imprint of a Kraken Tentacle'
+64350, -- 'Insect in Amber'
+64468, -- 'Proto-Drake Skeleton'
+66056, -- 'Shard of Petrified Wood'
+66057, -- 'Strange Velvet Worm'
+63527, -- 'Twisted Ammonite Shell'
+64387, -- 'Vicious Ancient Fish'
+};
+end
+
+local function ADB_NightElf(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+64646, -- 'Bones of Transformation'
+64361, -- 'Druid and Priest Statue Set'
+64358, -- 'Highborne Soul Mirror'
+64383, -- 'Kaldorei Wind Chimes'
+64643, -- 'Queen Azshara\'s Dressing Gown'
+64645, -- 'Tyrande\'s Favorite Doll'
+64651, -- 'Wisp Amulet'
+
+64647, -- 'Carcanet of the Hundred Magi'
+64379, -- 'Chest of Tiny Glass Animals'
+63407, -- 'Cloak Clasp with Antlers'
+63525, -- 'Coin from Eldre\'Thalas'
+64381, -- 'Cracked Crystal Vial'
+64357, -- 'Delicate Music Box'
+63528, -- 'Green Dragon Ring'
+64356, -- 'Hairpin of Silver and Malachite'
+63129, -- 'Highborne Pyxis'
+63130, -- 'Inlaid Ivory Comb'
+64354, -- 'Kaldorei Amphora'
+66055, -- 'Necklace with Elune Pendant'
+63131, -- 'Scandalous Silk Nightgown'
+64382, -- 'Scepter of Xavius'
+63526, -- 'Shattered Glaive'
+64648, -- 'Silver Scroll Case'
+64378, -- 'String of Small Pink Pearls'
+64650, -- 'Umbra Crescent'
+};
+end
+
+local function ADB_Nerubian(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+64481, -- 'Blessing of the Old God'
+64482, -- 'Puzzle Box of Yogg-Saron'
+
+64479, -- 'Ewer of Jormungar Blood'
+64477, -- 'Gruesome Heart Box'
+64476, -- 'Infested Ruby Ring'
+64475, -- 'Scepter of Nezar\'Azret'
+64478, -- 'Six-Clawed Cornice'
+64474, -- 'Spidery Sundial'
+64480, -- 'Vizier\'s Scrawled Streamer'
+};
+end
+
+local function ADB_Orc(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+64644, -- 'Headdress of the First Shaman'
+
+64436, -- 'Fiendish Whip'
+64421, -- 'Fierce Wolf Figurine'
+64418, -- 'Gray Candle Stub'
+64417, -- 'Maul of Stone Guard Mur\'og'
+64419, -- 'Rusted Steak Knife'
+64420, -- 'Scepter of Nekros Skullcrusher'
+64438, -- 'Skull Drinking Cup'
+64437, -- 'Tile of Glazed Clay'
+64389, -- 'Tiny Bronze Scorpion'
+};
+end
+
+local function ADB_Tolvir(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+60847, -- 'Crawling Claw'
+64881, -- 'Pendant of the Scarab Storm'
+64904, -- 'Ring of the Boy Emperor'
+64883, -- 'Scepter of Azj\'Aqir'
+64885, -- 'Scimitar of the Sirocco'
+64880, -- 'Staff of Ammunae'
+
+64657, -- 'Canopic Jar'
+64652, -- 'Castle of Sand'
+64653, -- 'Cat Statue with Emerald Eyes'
+64656, -- 'Engraved Scimitar Hilt'
+64658, -- 'Sketch of a Desert Palace'
+64654, -- 'Soapstone Scarab Necklace'
+64655, -- 'Tiny Oasis Mosaic'
+};
+end
+
+local function ADB_Troll(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+64377, -- 'Zin\'rokh, Destroyer of Worlds'
+
+64348, -- 'Atal\'ai Scepter'
+64346, -- 'Bracelet of Jade and Coins'
+63524, -- 'Cinnabar Bijou'
+64375, -- 'Drakkari Sacrificial Knife'
+63523, -- 'Eerie Smolderthorn Idol'
+63413, -- 'Feathered Gold Earring'
+63120, -- 'Fetish of Hir\'eek'
+66058, -- 'Fine Bloodscalp Dinnerware'
+64347, -- 'Gahz\'rilla Figurine'
+63412, -- 'Jade Asp with Ruby Eyes'
+63118, -- 'Lizard Foot Charm'
+64345, -- 'Skull-Shaped Planter'
+64374, -- 'Tooth with Gold Filling'
+63115, -- 'Zandalari Voodoo Doll'
+};
+end
+
+local function ADB_Vrykul(raceIndex)
+ArchDB_ArtifactList[raceIndex] = { 
+64460, -- 'Nifflevar Bearded Axe'
+
+64464, -- 'Fanged Cloak Pin'
+64462, -- 'Flint Striker'
+64459, -- 'Intricate Treasure Chest Key'
+64461, -- 'Scramseax'
+64467, -- 'Thorned Necklace'
+};
+end
+
+function ArchDB_ArtifactList_Setup(raceIndex, raceName)
+	if raceName == "Dwarf" then
+		ADB_Dwarf(raceIndex);
+	elseif raceName == "Draenei" then
+		ADB_Draenei(raceIndex);
+	elseif raceName == "Draenei" then
+		ADB_Draenei(raceIndex);
+	elseif raceName == "Fossil" then
+		ADB_Fossil(raceIndex);
+	elseif raceName == "Night Elf" then
+		ADB_NightElf(raceIndex);
+	elseif raceName == "Nerubian" then
+		ADB_Nerubian(raceIndex);
+	elseif raceName == "Orc" then
+		ADB_Orc(raceIndex);
+	elseif raceName == "Tol'vir" then
+		ADB_Tolvir(raceIndex);
+	elseif raceName == "Troll" then
+		ADB_Troll(raceIndex);
+	elseif raceName == "Vrykul" then
+		ADB_Vrykul(raceIndex);
+	end
+end
